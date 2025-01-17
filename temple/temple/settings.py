b4e191451de6a65d4190cca051ce8fcf637f1828
PrekshaAcharya5 <prekshaacharya57@gmail.com>
2025-01-17 15:25:44 +0545

    f

--- temple/temple/settings.py
+++ temple/temple/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-sjdfsdjfghjdghf
+xmnbxj
 import os
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
