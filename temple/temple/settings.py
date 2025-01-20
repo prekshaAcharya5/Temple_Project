5f0b8a902b5c0513a08c6a5fbf062f79f8fe3e55
PrekshaAcharya5 <prekshaacharya57@gmail.com>
2025-01-20 15:01:21 +0545

    Your commit message here

--- temple/temple/settings.py
+++ temple/temple/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-xmnbxj
 import os
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
@@ -257,6 +256,9 @@ USE_TZ = True
 import os
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
